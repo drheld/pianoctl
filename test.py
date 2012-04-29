ea8ad3f2ed69bd703370ce02467b8219b8f8af9d
David Held <drheld@gmail.com>
2012-04-29 13:46:24 -0400

    Remove test.py.
    This was just there to experiment on asynchronously controlling the
    pipe.

--- test.py
+++
@@ -1,8 +0,0 @@
-import fcntl
-import os
-import subprocess
-
-p = subprocess.Popen(['./pianobar'], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-fd = p.stdout.fileno()
-fl = fcntl.fcntl(fd, fcntl.F_GETFL)
-fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
