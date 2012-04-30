9c4798a5c0ba658eb27bfe3b0a2f9514a331cd3d
David Held <drheld@gmail.com>
2012-04-30 01:19:34 -0400

    Don't update browsers not active.

--- static/pianoctl.js
+++ static/pianoctl.js
@@ -1,3 +1,6 @@
+var focused = true;
+var running = false;
+
 $(document).ready(function() {
   // When the form  is clicked on, don't actually submit -- just send message.
   $('#form').submit(function() {
@@ -11,7 +14,6 @@ $(document).ready(function() {
     }
   });
 
-
   // Send command when buttons are clicked on.
   $('td').click(function() {
     $.post('ajax.html', { text: $(this).attr('command') });
@@ -19,9 +21,23 @@ $(document).ready(function() {
 
   // Defer waitForUpdate so android browser thinks page is fully lodaed.
   setTimeout('waitForUpdate()', '10');
+
+  $(window).blur(function() {
+    focused = false;
+  });
+  $(window).focus(function() {
+    focused = true;
+    if (!running) waitForUpdate();
+  });
 });
 
 function waitForUpdate() {
+  if (focused == false) {
+    running = false;
+    return;
+  }
+  running = true;
+
   $.ajax({
     type: "GET",
     url: "ajax.html",
